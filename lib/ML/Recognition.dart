df31f5da4f3155369202b00c14c34eb767844063
mlg2034 <ashatkabytaev@gmail.com>
2025-02-11 00:29:13 +0500

    init project

---
+++ lib/ML/Recognition.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class Recognition {
+  String name;
+  Rect location;
+  List<double> embeddings;
+  double distance;
+  Recognition(this.name, this.location,this.embeddings,this.distance);
+}
