953cbd506622ecdc817e169b2ebf30a1a12be322
mlg2034 <ashatkabytaev@gmail.com>
2025-05-11 11:55:02 +0500

    added firebase real time database

---
+++ lib/src/model/user_model.dart
@@ -0,0 +1,35 @@
+class UserModel {
+  final String name;
+  final String id;
+  final DateTime entryTime;
+  final DateTime exitTime;
+  final List<double> embeddings;
+  final int deviceId;
+
+  UserModel(
+      {required this.name,
+      required this.id,
+      required this.embeddings,
+      required this.entryTime,
+      required this.exitTime,
+      required this.deviceId});
+
+  factory UserModel.fromJson(Map<String, dynamic> json) {
+    return UserModel(
+      name: json['name'],
+      id: json['id'],
+      embeddings: json['embeddings'],
+      entryTime: json['entryTime'],
+      exitTime: json['exitTime'],
+      deviceId: json['deviceId'],
+    );
+  }
+
+  Map<String, dynamic> toData() => {
+        'name': name,
+        'id': id,
+        'entryTime': entryTime,
+        'exitTime': exitTime,
+        'embeddings': embeddings
+      };
+}
